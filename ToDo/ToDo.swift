63cca25fdc6d7cf0ba6820cc6ff3734697d0cdaf
Hana Stauss <hanastauss@icloud.com>
2020-06-04 17:13:40 -0700

    Complete Iteration 0

---
+++ ToDo/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDo
+//
+//  Created by Hana Stauss on 6/4/20.
+//  Copyright © 2020 Hana Stauss. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
